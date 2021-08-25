39126a612e217b9f2202945a653f67b2de2671be
swillrodt <sumnerwillrodt@gmail.com>
2021-08-24 23:12:11 -0700

    introduced functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+//window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+//an alert is a way to get into to the user-not the developer
+//window.alert(playerName);
+
+console.log(playerName); //for developers eyes only
+
+//fight function
+function fight () {
+    window.alert("The fight has begun!");
+}
+//fight();
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
